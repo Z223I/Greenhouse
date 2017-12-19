5065f0b479db6269ea27a101d64ffdb4dafb219f
Z223I <BruceWilsonPH@yahoo.com.ph>
2017-12-18 19:34:32 -0600

    A little test code.

---
+++ test.py
@@ -0,0 +1,20 @@
+from DS18B20 import DS18B20
+import time
+
+
+therms = DS18B20()
+
+therms.NameDevice( "Air" )
+therms.NameDevice( "Water" )
+therms.create_dict()
+
+while True:
+    water_temp = therms.get_current_temp( "Water" )
+    print "Water temp: ", water_temp
+
+    air_temp = therms.get_current_temp( "Air" )
+    print "Air temp: ", air_temp
+
+    print
+
+    time.sleep(2)
